bc1756ade9845575e2ddbf59b3b543c036fe32dd
laurenseliski <laurenseliski37@gmail.com>
2024-04-26 06:38:40 -0400

    Rearranging and simplifying
    Instead of one large function with nested ifs, started breaking it down into simple smaller functions. Getting confused on naming because they all sound so similar...

--- saveyourhouse.py
+++ saveyourhouse.py
@@ -1,10 +1,12 @@
-
 #import words from wordlist.py and import tkinter
 import tkinter as tk
 import random
 from wordlist import word_list
 
-#add house art
+#house art filler
+house_art = [
+    "|/|\|"
+]
 
 def choose_word():
     return random.choice(word_list)
@@ -12,17 +14,86 @@ def choose_word():
 def update_house(tries):
     house_label.config(text=house_art[tries])
 
-#main app window 
+def play_game(guess):
+    global guessed, tries, word, word_completion
+    guessed = False
+    tries = 0
+    word = choose_word()
+    word_completion = "_"* len(word)
+    word_label.config(text=word_completion)
+
+def guess_type(guess):
+    if len(guess) == 1 and guess.isalpha():
+        return "letter", guess.upper()
+    elif len(guess) == len(word) and guess.isalpha():
+        return "word", guess.upper()
+    else:
+        return "invalid", None
+    
+guessed_letters = []
+
+def check_guess(letter_or_word, guess):
+
+    global tries, guessed, word_completion
+
+    if letter_or_word == "letter":
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                update_house(tries)
+                tries += 1
+                guessed_letters.append(guess)
+            else:
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+    elif len(guess) == len(word) and guess.isalpha():
+            guess = guess.upper()
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries += 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+    else:
+        print("Not a valid guess.")
+    print(update_house(tries))
+    print(word_completion)
+    print("\n")
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+
+def play_game(guess):
+    global guessed, tries, word, word_completion
+    guessed = False
+    tries = 0
+    word = choose_word()
+    word_completion = "_"* len(word)
+    word_label.config(text=word_completion)
+
+def make_guess():
+    guess = input("Enter your guess: ")
+    guess_type, guess = check_guess(guess)
+    message, game_over = check_guess(guess_type, guess)
+    status_label.config(text=message)
+    if game_over:
+        guess_button.config(state=tk.DISABLED)
+
+#pop-up window
 root = tk.Tk()
 root.title("Save Your House")
 
-word = ""
-word_completion = ""
-guessed_letters = []
-guessed_words = []
-tries = 6
-
-#Gui components
+#GUI components:
 
 #displays the house picture
 house_label = tk.Label(root,font=("Times New Roman", 12))
@@ -33,7 +104,7 @@ word = choose_word()
 word_label=tk.Label(root, text= "_"*len(word), font=("Times New Roman", 16))
 word_label.grid(row=1, column=0)
 
-# displays player input
+#displays player input
 guess_entry = tk.Entry(root, width=3, font=("Times New Roman", 16))
 guess_entry.grid(row=2, column=0)
 
@@ -45,9 +116,9 @@ guess_button.grid(row=2, column=1)
 status_label = tk.Label(root, font=("Times New Roman", 16))
 status_label.grid(row=3, column=0)
 
-update_house(tries)
+play_game()
 
-#root.mainloop()
+root.mainloop()
 
 #player can request a hint
 #hint_request = tk.Label(root,text="")
@@ -56,82 +127,3 @@ update_house(tries)
 #hint category is displayed
 #hint = tk.Label(root,text="")
 #hint.pack()
-
-def get_word():
-    word =random.choice(word_list)
-    return word.upper()
-
-def play(word):
-    word_completion = "_" * len(word)
-    guessed = False
-    guessed_letters = []
-    guessed_words = []
-    tries = 6
-    print("Hurry! You need to guess the word to save your house!")
-    print(display_house(tries))
-    print(word_completion)
-    print("\n")
-    while not guessed and tries > 0:
-        guess = input("Please guess a letter : ")
-        guess = guess.upper()
-        if len(guess) == 1 and guess.isalpha():
-            if guess in guessed_letters:
-                print("You already guessed the letter", guess)
-            elif guess not in word:
-                print(guess, "is not in the word.")
-                tries -= 1
-                guessed_letters.append(guess)
-            else:
-                print("Good job,", guess, "is in the word!")
-                guessed_letters.append(guess)
-                word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(word) if letter == guess]
-                for index in indices:
-                    word_as_list[index] = guess
-                word_completion = "".join(word_as_list)
-                if "_" not in word_completion:
-                    guessed = True
-        elif len(guess) == len(word) and guess.isalpha():
-            guess = guess.upper()
-            if guess in guessed_words:
-                print("You already guessed the word", guess)
-            elif guess != word:
-                print(guess, "is not the word.")
-                tries -= 1
-                guessed_words.append(guess)
-            else:
-                guessed = True
-                word_completion = word
-        else:
-            print("Not a valid guess.")
-        print(display_house(tries))
-        print(word_completion)
-        print("\n")
-    if guessed:
-        print("Congrats, you guessed the word! You win!")
-    else:
-        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
-
-def initialize_game():
-    global word, word_completion, guessed_letters, guessed_words, tries_remain
-    word = get_word()
-    word_completion = "_"*len(word)
-    guessed_letters = []
-    guessed_words = []
-    tries = 6
-    update_house(tries)
-
-def make_guess():
-    global word, word_completion, guesed_letters, guessed_words, tries
-    guess = guess_entry.get().upper()
-
-def main():
-    word = get_word()
-    play(word)
-    while input("Play Again? (Y/N) ").upper() == "Y":
-        word = get_word()
-        play(word)
-
-
-if __name__ == "__main__":
-    main()
